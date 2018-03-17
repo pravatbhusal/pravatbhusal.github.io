09c66f6e7d74fce5f2fbfe6106ea87c3312658d2
Pravat Bhusal <pravat.bhusal@gmail.com>
2018-03-17 15:41:05 -0500

    Add a navbar and about content page

---
+++ blog.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+  <head>
+    <title>Pravat Bhusal</title>
+    <!--style sheets-->
+		<link href="styles/blogstyles.css" type="text/css" rel="stylesheet">
+  </head>
+  <main>
+  </main>
+  <footer>
+  </footer>
+</body>
+
+</html>
