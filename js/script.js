5a172caf7d686a1103031d20b379c4f7be3a4366
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-05-07 02:49:39 -0500

    Remove skill set and use the icon instead
    We cannot quantify my actual skill.

--- js/script.js
+++ js/script.js
@@ -19,25 +19,6 @@ function updateCanvasText() {
 }
 updateCanvasText();
 
-//progress bars
-var html5css3Bar = new RadialProgress(document.getElementById("skills-skill-html5css3"),
-{progress:0.80, colorFg:"#E4512A", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
-var javascriptBar = new RadialProgress(document.getElementById("skills-skill-javascript"),
-{progress:0.65, colorFg:"#F0DB4F", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
-var mysqlBar = new RadialProgress(document.getElementById("skills-skill-mysql"),
-{progress:0.55, colorFg:"#00618A", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
-var pythonBar = new RadialProgress(document.getElementById("skills-skill-python"),
-{progress:0.40, colorFg:"#F8B721", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
-var javaBar = new RadialProgress(document.getElementById("skills-skill-java"),
-{progress:0.40, colorFg:"#F69314", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
-var phpBAR = new RadialProgress(document.getElementById("skills-skill-php"),
-{progress:0.40, colorFg:"#777BB3", colorBg:"grey", thick:2.5, fixedTextSize:0.3});
-
 //google maps
 var myLatLng = {lat: 30.2849, lng: -97.7341};
 var map = new google.maps.Map(document.getElementById('google-maps'), {
